1e149081b6d0cbefb7bc9d5c3240ba4588279ef7
FenrisSVG <fsandovalchavez5@gmail.com>
2023-05-18 06:52:44 -0600

    new commit may 2023

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1200,
   viewportHeight: 720,
+  video: true,
+  videoCompression: 32,
+  videoUploadOnPasses: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/test-advice.cy.ts
+++ cypress/e2e/test-advice.cy.ts
@@ -2,6 +2,8 @@ describe('Should visit and show the page', () => {
   //Prueba para verificar si la webpage esta visible al usuario.
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
+    //cy.mount(<Layout />);
+    //cy.mount(<Main />);
   })
 
   it('user can see the page', () => {
@@ -16,8 +18,13 @@ describe('Should visit and show the page', () => {
 
   it('API Should be OK Response', () => {
     cy.log('Test response API')
+    /**
+     * @returns El estado del end point de la api (200) tanto como si el estado es "Ok".
+     * @slip_id Numero aleatorio a ser usado como parametro de API.  
+     */
     let slip_id = Math.floor(Math.random() * 150);
     const API_ENDPOINT = `https://api.adviceslip.com/advice/${slip_id}`;
+
     cy.request('GET',API_ENDPOINT).then(res => {
       //Problamos el estado 200 de la API.
       expect(res.status).to.equal(200)
@@ -30,14 +37,14 @@ describe('Should visit and show the page', () => {
     cy.log('Prueba clickear boton generar un nuevo consejo aleatorio')
     // Identificamos el boton y le decimos que de click!
     cy.get('button').should('be.visible').trigger('click')
-    //
+
     cy.log('Probamos que el boton tenga la clase css de flex')
     cy.get('[data-test-id="advice-button"]').should(
       'have.css',
       'display',
       'flex'
     )
-      //
+
     cy.log('Probamos que el tailwind este funcionando verificando que tenga la clase del bg')
     cy.get('[data-test-id="advice-button"]').should(
       'have.class',
@@ -59,5 +66,27 @@ describe('Should visit and show the page', () => {
      * ! NOS DARIA ERROR
      */
     cy.title().should('eq','Advice generator app coding.')
+  })
+
+  it('Pruebas Unitarias Advice Generator', () => {
+    cy.log('Escenarios (2) de pruebas Unitarias')
+    /**
+     * Prueba Unitaria #1 Hacer click al botón la API nos devuelve texto.
+     */
+    cy.get('[data-test-id="advice-button"]').trigger('click')
+    cy.get('[data-test-id="advice-body"]').then((text) => {
+      expect(text.length).to.be.at.least(1)
+    }) 
+    /**
+     * Prueba Unitaria #2
+     */
+    let slip_id = Math.floor(Math.random() * 150);
+    const API_ENDPOINT = `https://api.adviceslip.com/advice/${slip_id}`;
+
+    cy.request('GET',API_ENDPOINT).as('advices')
+    cy.get('@advices').then((res) => {
+      //cy.log(JSON.stringify(res.slip));
+      expect(Number.isNaN(slip_id), slip_id.toString()).to.eq(false)
+    })
   })
 })
